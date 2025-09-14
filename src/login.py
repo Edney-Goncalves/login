754761cf0afe6aeb3bfc423c6f0feda91ff53160
Edney Gonçalves <dneylele@gmail.com>
2025-09-14 05:27:10 -0300

    nada importante

--- src/login.py
+++ src/login.py
@@ -13,6 +13,8 @@ def carregar_usuarios():
     return{}
 
 def salvar_usuarios(usuarios):
+    
+    os.makedirs(os.path.dirname(ARQUIVO_USUARIOS), exist_ok=True)
     with open(ARQUIVO_USUARIOS, 'w') as f:
         json.dump(usuarios, f, indent=4)
         
@@ -20,40 +22,50 @@ def salvar_usuarios(usuarios):
 def main():
     usuarios = carregar_usuarios()
     
-    print('Bem vindo à página de login')
-    #opções para o usuário
-    print('Login [ 1 ]')
-    print('Novo usuário [ 2 ]')
-    opcao = input('O que você deseja fazer? ')
+    while True:
+        print('Login [ 1 ]')
+        print('Cadastre-se [ 2 ]')
+        opcao = input(' ')
 
-    # se ele digitar '1', será remetido para a página de login
-    if opcao == "1":
-        usuario = input('Usuário: ')
-        senha = input('Senha: ')
-        if usuarios.get(usuario) == senha:
-            print(f'Bem vindo, {usuario}!')
-        else:
-            print(f'Usuário ou senha incorretos.')
+        # se ele digitar '1', será remetido para a página de login;
+        # Enquanto Usuário/Senha não estiver no banco de dados ou for digitado 
+        # incorretamente, ficará nesse laço
+        if opcao == "1":
+            usuario = input('Usuário: ')
+            senha = input('Senha: ')
+            if usuarios.get(usuario) == senha:
+                print(f'Bem vindo, {usuario}!')
+                print("FIM DE PROGRAMA")
+                break
+            else:
+                print(f'Usuário ou senha incorretos.')
+                
+        
+        # caso digite a opção '2', será encaminhado para a página de criação de perfil.
+        elif opcao == "2":
+            novo_usuario = input('Nome do usuário: ').strip
             
-    
-    # caso digite a opção '2', será encaminhado para a página de criação de perfil.
-    elif opcao == "2":
-        novo_usuario = input('Nome do usuário: ')
-        
-        # caso o nome digitado em novo_usuario já estiver em usuarios:
-        if novo_usuario in usuarios:
-            print('Usuário já cadastrado.')
-            return
-        
-        nova_senha = input('Crie sua senha: ')
-        confirma_senha = input('Confirme sua senha: ')
-        
-        if nova_senha != confirma_senha:
-            print('Senhas não coincidem. Digite novamente: ')
+            if not novo_usuario:
+                print("Nome de usuário está vazio.")
+                continue
+            # caso o nome digitado em novo_usuario já estiver em usuarios:
+            elif novo_usuario in usuarios:
+                print('Usuário já cadastrado.')
+                continue
             
-        usuarios[novo_usuario] = nova_senha
-        salvar_usuarios(usuarios)
-        print('Usuário criado com sucesso.')            
+            nova_senha = input('Crie sua senha: ')
+            confirma_senha = input('Confirme sua senha: ')
+            
+            if nova_senha != confirma_senha:
+                print('Senhas não coincidem. Digite novamente: ')
+                nova_senha = input('Crie sua senha: ')
+                confirma_senha = input('Confirme sua senha: ')
+                continue
+        
+            
+            usuarios[novo_usuario] = nova_senha
+            salvar_usuarios(usuarios)
+            print('Usuário criado com sucesso.')            
 
 if __name__== "__main__":
     main()
